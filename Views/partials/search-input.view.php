694ccf7d6e66bf709ae9d4bf0aad5f0a93a46c6b
Abraham LC <alc_inf@yahoo.com.mx>
2023-05-25 11:06:59 -0600

    Start web-scraper

---
+++ Views/partials/search-input.view.php
@@ -0,0 +1,4 @@
+<form action="search">
+    <input type="text" class="search-input" name="keywords" placeholder="Write that you're looking for...">
+    <button type="submit" class="search-button">Search</button>
+</form>
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'type' => 'mysql',
+        'host' => '127.0.0.1',
+        'database' => 'web_scraper',
+        'user' => 'root',
+        'password' => '1234',
+    ],
+    'error_handling' => true,
+];
